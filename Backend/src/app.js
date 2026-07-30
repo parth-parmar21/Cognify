ec6df8e3e7ce316ae738da3439c0c4c29c1642a8
parth-parmar21 <parthparmar3663@gmail.com>
2026-07-31 02:06:03 +0530

    fix: update API base URLs to use production server

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://cognify-alpha-six.vercel.app"
+    ],
     credentials: true,
 }))
 import authRouter from './routes/auth.routes.js'
