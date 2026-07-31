d0aaff8b049cde8dae98294207a74e70d457f8af
parth-parmar21 <parthparmar3663@gmail.com>
2026-07-31 19:09:31 +0530

    debugging

--- Frontend/src/features/chat/service/chat.socket.js
+++ Frontend/src/features/chat/service/chat.socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
 export function initializedSocketConnection() {
+    console.log("Backend URL from chat:", import.meta.env.VITE_BACKEND_URL);
     const socket = io(`${import.meta.env.VITE_BACKEND_URL}`, {
         withCredentials: true,
     });
