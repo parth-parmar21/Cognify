7fb385ffda6cd75d46d39dbb63e2cf49dd8597ec
parth-parmar21 <parthparmar3663@gmail.com>
2026-07-31 15:12:25 +0530

    fix: simplify Gmail transport configuration by removing unnecessary parameters

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,16 +1,11 @@
 import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
+    service: "gmail",
     auth: {
         type: "OAuth2",
         user: process.env.GOOGLE_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+        pass: process.env.GOOGLE_APP_PASSWORD
     },
 });
 
